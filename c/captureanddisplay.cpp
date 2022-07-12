2f3d378323290bc39f3304c49c8a7ab8f991329e
daniel_tisza <daniel.tisza@tietolaite.com>
2022-07-12 07:23:38 +0300

    Add captureanddisplay.cpp

---
+++ c/captureanddisplay.cpp
@@ -0,0 +1,101 @@
+/*-----------------------------------------------------------
+-- captureanddisplay.c
+--
+-- Capture image and display it
+-- Based on SingleCapture.cpp
+--
+-- g++ captureanddisplay.cpp -I/opt/mvIMPACT_Acquire -L/opt/mvIMPACT_Acquire/lib/armhf -l mvDeviceManager -o captureanddisplay
+--
+-- Copyright: Daniel Tisza, 2022, GPLv3 or later
+-----------------------------------------------------------*/
+
+#include <iostream>
+#include <apps/Common/exampleHelper.h>
+#include <mvIMPACT_CPP/mvIMPACT_acquire.h>
+
+using namespace mvIMPACT::acquire;
+using namespace std;
+
+//-----------------------------------------------------------------------------
+int main( void )
+//-----------------------------------------------------------------------------
+{
+    DeviceManager devMgr;
+    Device* pDev = getDeviceFromUserInput( devMgr );
+    if( !pDev )
+    {
+        cout << "Unable to continue! Press [ENTER] to end the application" << endl;
+        cin.get();
+        return 1;
+    }
+
+    try
+    {
+        pDev->open();
+    }
+    catch( const ImpactAcquireException& e )
+    {
+        // this e.g. might happen if the same device is already opened in another process...
+        cout << "An error occurred while opening the device(error code: " << e.getErrorCode() << ")." << endl
+             << "Press [ENTER] to end the application" << endl;
+        cin.get();
+        return 1;
+    }
+
+    FunctionInterface fi( pDev );
+
+    // send a request to the default request queue of the device and wait for the result.
+    fi.imageRequestSingle();
+
+    manuallyStartAcquisitionIfNeeded( pDev, fi );
+
+    // Wait for results from the default capture queue by passing a timeout (The maximum time allowed
+    // for the application to wait for a Result). Infinity value: -1, positive value: The time to wait in milliseconds.
+    // Please note that slow systems or interface technologies in combination with high resolution sensors
+    // might need more time to transmit an image than the timeout value.
+    // Once the device is configured for triggered image acquisition and the timeout elapsed before
+    // the device has been triggered this might happen as well.
+    // If waiting with an infinite timeout(-1) it will be necessary to call 'imageRequestReset' from another thread
+    // to force 'imageRequestWaitFor' to return when no data is coming from the device/can be captured.
+    int requestNr = fi.imageRequestWaitFor( 10000 );
+
+    manuallyStopAcquisitionIfNeeded( pDev, fi );
+
+    // check if the image has been captured without any problems.
+    if( !fi.isRequestNrValid( requestNr ) )
+    {
+        // If the error code is -2119(DEV_WAIT_FOR_REQUEST_FAILED), the documentation will provide
+        // additional information under TDMR_ERROR in the interface reference
+        cout << "imageRequestWaitFor failed maybe the timeout value has been too small?" << endl;
+        return 1;
+    }
+
+    const Request* pRequest = fi.getRequest( requestNr );
+
+    if( !pRequest->isOK() )
+    {
+        cout << "Error: " << pRequest->requestResult.readS() << endl;
+        // if the application wouldn't terminate at this point this buffer HAS TO be unlocked before
+        // it can be used again as currently it is under control of the user. However terminating the application
+        // will free the resources anyway thus the call
+        // fi.imageRequestUnlock( requestNr );
+        // can be omitted here.
+        return 1;
+    }
+
+    cout    << "Image captured(" 
+            << pRequest->imagePixelFormat.readS() 
+            << " " 
+            << pRequest->imageWidth.read() 
+            << "x" 
+            << pRequest->imageHeight.read() 
+            << ")" 
+            << endl;
+
+    // unlock the buffer to let the driver know that you no longer need this buffer.
+    fi.imageRequestUnlock( requestNr );
+
+    cout << "Press [ENTER] to end the application" << endl;
+    cin.get();
+    return 0;
+}
