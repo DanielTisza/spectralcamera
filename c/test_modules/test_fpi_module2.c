9fd515c054ae71f0920483cd684dbb6968460815
daniel_tisza <daniel.tisza@tietolaite.com>
2022-07-20 16:41:58 +0300

    Change to use the MFPI setting for 4 selected wavelengths (554, 600, 695, 800).

--- c/test_modules/test_fpi_module2.c
+++ c/test_modules/test_fpi_module2.c
@@ -10,69 +10,6 @@
 #
 # ----------------------------------------------------------------------------*/
 
-#if 0
-import fpipy as fp
-
-from spectracular.fpi_driver import detectFPIDevices, createFPIDevice
-		
-FPI_IDS = [
-    # ( VID,   PID) (and the same in decimal)
-    ('1FC9', '0083'), (8137, 131),
-    ]
-"""Known VID:PID pairs of FPI devices."""
-FPI_HWIDS = [
-    # Strings returned by read_hardware_id
-	 'd02b012 af380065 5b5bbeab f50019c1'
-]
-
-fpi = createFPIDevice(detectFPIDevices(FPI_IDS, FPI_HWIDS)[0].device)
-print(fpi)
-
-print("_hw_id: " + str(fpi._hw_id))
-
-print("_hw_info: [" + str(fpi._hw_info) + "]")
-
-print("_eeprom: ")
-print(fpi._eeprom)
-
-# MFPI specific
-print("_expected_settling_time: " + str(fpi._expected_settling_time))
-print("_setpoint_cmd: " + str(fpi._setpoint_cmd))
-print("_current_setpoint: " + str(fpi._current_setpoint))
-
-# MFPI parseEeprom()
-print("_is_AC: " + str(fpi._is_AC))
-print("_name: " + str(fpi._name))
-print("_eeprom_id: " + str(fpi._eeprom_id))
-print("_dn_max: " + str(fpi._dn_max))
-print("_dn_min: " + str(fpi._dn_min))
-
-# MFPI getTemperature()
-print("getTemperature(): " + str(fpi.getTemperature()))
-
-# MFPI setSetpoint()
-
-# 3	72	1	52164	0	0	554.0473012	0	0	-0.167038819	0.897291038	-1.106291748	0	0	0	0	0	0	20.36692413	0	0
-# 3	112	1	46815	0	0	600.3957949	0	0	0.63670834	0.421133211	-0.719467142	0	0	0	0	0	0	22.66214846	0	0
-# 4	121	1	53506	0	0	695.9738452	0	0	0.746581945	0.746581945	0.746581945	0	0	0	0	0	0	30.93814177	0	0
-# 6	190	1	44248	0	0	800.8814747	0	0	0.930350147	0.930350147	0.930350147	0	0	0	0	0	0	25.21518763	0	0
-
-print("_current_setpoint: " + str(fpi._current_setpoint))
-
-fpi.setSetpoint(52164)
-print("_current_setpoint: " + str(fpi._current_setpoint))
-
-fpi.setSetpoint(46815)
-print("_current_setpoint: " + str(fpi._current_setpoint))
-
-fpi.setSetpoint(53506)
-print("_current_setpoint: " + str(fpi._current_setpoint))
-
-fpi.setSetpoint(44248)
-print("_current_setpoint: " + str(fpi._current_setpoint))
-
-#endif
-
 //#include <iostream>
 //using namespace std;
 
@@ -94,26 +31,26 @@ long		dnMax;
 long		dnMin;
 
 /*
-ledset	index	Npeaks	SP1	SP2	SP3	PeakWL1	PeakWL2	PeakWL3	Sinv11	Sinv12	Sinv13	Sinv21	Sinv22	Sinv23	Sinv31	Sinv32	Sinv33	FWHM1	FWHM2	FWHM3
-0	0	2	44748	0	0	505.914289	614.2813052	0	-0.398411474	1.259903596	1.025911637	0.752656263	-0.099247137	0.020958335	0	0	0	14.26460175	22.90707427	0
-0	1	2	38137	0	0	529.6967931	648.7050781	0	-0.213236666	0.675034929	0.47800432	1.096529239	-0.196707289	0.090149999	0	0	0	19.31133749	22.91851783	0
-0	2	1	51545	0	0	559.946926	0	0	0.679889519	1.419721666	-4.845915769	0	0	0	0	0	0	22.17152035	0	0
-0	3	1	48253	0	0	590.5393079	0	0	0.744158985	0.88514816	-1.525211353	0	0	0	0	0	0	19.27394234	0	0
-0	4	2	43666	0	0	509.0977574	620.8469159	0	-0.278237594	0.95500646	0.689725545	0.799175554	-0.12899134	0.070664975	0	0	0	17.24490964	22.21616876	0
-0	5	2	32410	0	0	547.9435618	670.419621	0	-0.033097435	1.002178578	-1.246062	0.941021878	-0.658921908	2.815570398	0	0	0	17.5773986	22.00005834	0
-0	6	1	52670	0	0	709.6242043	0	0	0.761968164	0.761968164	0.761968164	0	0	0	0	0	0	27.34661845	0	0
-0	7	1	50362	0	0	740.2281587	0	0	0.810986945	0.810986945	0.810986945	0	0	0	0	0	0	28.68050028	0	0
+# 3	72	1	52164	0	0	554.0473012	0	0	-0.167038819	0.897291038	-1.106291748	0	0	0	0	0	0	20.36692413	0	0
+# 3	112	1	46815	0	0	600.3957949	0	0	0.63670834	0.421133211	-0.719467142	0	0	0	0	0	0	22.66214846	0	0
+# 4	121	1	53506	0	0	695.9738452	0	0	0.746581945	0.746581945	0.746581945	0	0	0	0	0	0	30.93814177	0	0
+# 6	190	1	44248	0	0	800.8814747	0	0	0.930350147	0.930350147	0.930350147	0	0	0	0	0	0	25.21518763	0	0
 */
 
-long setpoint[8] = {
-	44748,
-	38137,
-	51545,
-	48253,
-	43666,
-	32410,
-	52670,
-	50362,
+#define SETPOINT_COUNT		4
+
+int ledset[SETPOINT_COUNT] = {
+	3,
+	3,
+	4,
+	6,
+};
+
+long setpoint[SETPOINT_COUNT] = {
+	52164,
+	46815,
+	53506,
+	44248,
 };
 
 void fpi_command(
@@ -304,7 +241,7 @@ void main(
 	/*
 	 * FPI set setpoints
 	 */
-	for (ii=0;ii<8;ii++) {
+	for (ii=0;ii<SETPOINT_COUNT;ii++) {
 
 		printf("Set FPI setpoint: %d\r\n", setpoint[ii]);
 		printf("Press to activate\r\n");
