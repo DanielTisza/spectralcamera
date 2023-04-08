de881c62b7ab4245e01c11b8ad13476efe4c6f70
daniel_tisza <daniel.tisza@tietolaite.com>
2023-04-08 09:40:11 +0300

    Change to instantiate rowdelayram component.

--- vhdl/rowdelay.vhd
+++ vhdl/rowdelay.vhd
@@ -53,6 +53,27 @@ end rowdelay;
 -- Describe the contents of this "chip"
 architecture rtl of rowdelay is
 
+	component rowdelayram is
+
+		generic(
+			C_M_AXI_ADDR_WIDTH	: integer	:= 32;
+			C_M_AXI_DATA_WIDTH	: integer	:= 64
+		);
+	
+		port (
+	
+			-- Clock and reset
+			clk : in std_logic;
+	
+			-- RAM signals
+			wr : in std_logic;
+			addr : in std_logic_vector(9 downto 0);
+			wr_data : in std_logic_vector(47 downto 0);
+			rd_data : out std_logic_vector(47 downto 0)
+		);
+	
+	end component rowdelayram;
+
 	-- Direct read
 	signal src1A : unsigned(C_M_AXI_DATA_WIDTH-1 downto 0);
 	signal src2A : unsigned(C_M_AXI_DATA_WIDTH-1 downto 0);
@@ -91,45 +112,28 @@ architecture rtl of rowdelay is
 	-- 2590/4 = 647.5 = 648
 
 	subtype ram_word_type is std_logic_vector(47 downto 0);
-	type ram_type is array(0 to 647) of ram_word_type ;
 	subtype ram_addr_type is unsigned(9 downto 0);
 	
 	-- RAM1 signals
-	signal ram1 : ram_type;
 	signal ram1_addr : ram_addr_type;
 	signal ram1_rd_data : ram_word_type;
 	signal ram1_wr_data : ram_word_type;
 	signal ram1_wr : std_logic;
 
 begin
-	
-	------------------------------------------
-	-- RAM with single read and write address
-	------------------------------------------
-	ram1_proc : process(
-		clk,
-		ram1,
-		ram1_addr,
-		ram1_wr_data,
-		ram1_wr
-	)
-	begin
-		
-		if (clk'event and clk='1') then
-		
-			-- Write to RAM
-			if (ram1_wr='1') then
-				ram1(to_integer(ram1_addr)) <= ram1_wr_data;
-			else
-			end if;
-			
-			-- Read from RAM
-			ram1_rd_data <= ram1(to_integer(ram1_addr));
-		
-		else
-		end if;
-	end process;
 
+	rowdelayram1 : rowdelayram port map(
+
+		clk => clk,
+	
+		-- RAM signals
+		wr => read_done_a,
+		addr => std_logic_vector(ram1_addr),
+		wr_data => ram1_wr_data,
+		rd_data => ram1_rd_data
+	);
+	
+	ram1_addr <= to_unsigned(0,10);
 	ram1_wr_data <= read_data(59 downto 48) & read_data(43 downto 32) & read_data(27 downto 16) & read_data(11 downto 0);
 	ram1_wr <= read_done_a;
 	src1A <= unsigned(ram1_rd_data);
