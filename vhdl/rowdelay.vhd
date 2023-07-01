e9ed35a6764e7a8e730a39ecf1e445a56954d55d
daniel_tisza <daniel.tisza@tietolaite.com>
2023-07-01 08:13:02 +0300

    Cleanup rowdelay.vhd, move calculation to other blocks.

--- vhdl/rowdelay.vhd
+++ vhdl/rowdelay.vhd
@@ -41,21 +41,31 @@ entity rowdelay is
 		vsync_ena : in std_logic;
 
 		-- Output data signals
-		respix1r : out unsigned(11 downto 0);
-		respix1g : out unsigned(11 downto 0);
-		respix1b : out unsigned(11 downto 0);
+		targetpix1r : out unsigned(11 downto 0);
+		targetpix1g : out unsigned(11 downto 0);
+		targetpix1b : out unsigned(11 downto 0);
+		targetpix2r : out unsigned(11 downto 0);
+		targetpix2g : out unsigned(11 downto 0);
+		targetpix2b : out unsigned(11 downto 0);
+		targetpix3r : out unsigned(11 downto 0);
+		targetpix3g : out unsigned(11 downto 0);
+		targetpix3b : out unsigned(11 downto 0);
+		targetpix4r : out unsigned(11 downto 0);
+		targetpix4g : out unsigned(11 downto 0);
+		targetpix4b : out unsigned(11 downto 0);
 
-		respix2r : out unsigned(11 downto 0);
-		respix2g : out unsigned(11 downto 0);
-		respix2b : out unsigned(11 downto 0);
-
-		respix3r : out unsigned(11 downto 0);
-		respix3g : out unsigned(11 downto 0);
-		respix3b : out unsigned(11 downto 0);
-
-		respix4r : out unsigned(11 downto 0);
-		respix4g : out unsigned(11 downto 0);
-		respix4b : out unsigned(11 downto 0)
+		whitepix1r : out unsigned(11 downto 0);
+		whitepix1g : out unsigned(11 downto 0);
+		whitepix1b : out unsigned(11 downto 0);
+		whitepix2r : out unsigned(11 downto 0);
+		whitepix2g : out unsigned(11 downto 0);
+		whitepix2b : out unsigned(11 downto 0);
+		whitepix3r : out unsigned(11 downto 0);
+		whitepix3g : out unsigned(11 downto 0);
+		whitepix3b : out unsigned(11 downto 0);
+		whitepix4r : out unsigned(11 downto 0);
+		whitepix4g : out unsigned(11 downto 0);
+		whitepix4b : out unsigned(11 downto 0)
 
 	);
 
@@ -63,49 +73,7 @@ end rowdelay;
 
 -- Describe the contents of this "chip"
 architecture rtl of rowdelay is
-
-	component base_zynq_floating_point_0_0 is
-	port (
-		aclk : in STD_LOGIC;
-		s_axis_a_tvalid : in STD_LOGIC;
-		s_axis_a_tready : out STD_LOGIC;
-		s_axis_a_tdata : in STD_LOGIC_VECTOR ( 15 downto 0 );
-		m_axis_result_tvalid : out STD_LOGIC;
-		m_axis_result_tready : in STD_LOGIC;
-		m_axis_result_tdata : out STD_LOGIC_VECTOR ( 63 downto 0 )
-	);
-	end component base_zynq_floating_point_0_0;
-
-	component base_zynq_floating_point_1_0 is
-	port (
-		aclk : in STD_LOGIC;
-		s_axis_a_tvalid : in STD_LOGIC;
-		s_axis_a_tready : out STD_LOGIC;
-		s_axis_a_tdata : in STD_LOGIC_VECTOR ( 63 downto 0 );
-		s_axis_b_tvalid : in STD_LOGIC;
-		s_axis_b_tready : out STD_LOGIC;
-		s_axis_b_tdata : in STD_LOGIC_VECTOR ( 63 downto 0 );
-		m_axis_result_tvalid : out STD_LOGIC;
-		m_axis_result_tready : in STD_LOGIC;
-		m_axis_result_tdata : out STD_LOGIC_VECTOR ( 63 downto 0 )
-	);
-	end component base_zynq_floating_point_1_0;
 	
-	component base_zynq_floating_point_2_0 is
-	port (
-		aclk : in STD_LOGIC;
-		s_axis_a_tvalid : in STD_LOGIC;
-		s_axis_a_tready : out STD_LOGIC;
-		s_axis_a_tdata : in STD_LOGIC_VECTOR ( 63 downto 0 );
-		s_axis_b_tvalid : in STD_LOGIC;
-		s_axis_b_tready : out STD_LOGIC;
-		s_axis_b_tdata : in STD_LOGIC_VECTOR ( 63 downto 0 );
-		m_axis_result_tvalid : out STD_LOGIC;
-		m_axis_result_tready : in STD_LOGIC;
-		m_axis_result_tdata : out STD_LOGIC_VECTOR ( 63 downto 0 )
-	);
-	end component base_zynq_floating_point_2_0;
-
 	------------------------------------------
 	-- rowdelayram
 	------------------------------------------
@@ -300,101 +268,6 @@ architecture rtl of rowdelay is
 	signal whitedmpix4g : unsigned(11 downto 0);
 	signal whitedmpix4b : unsigned(11 downto 0);
 
-	-- Inversion coefficients
-	-- These will be fixed-point fractional values
-	signal sinvr : unsigned(11 downto 0);
-	signal sinvg : unsigned(11 downto 0);
-	signal sinvb : unsigned(11 downto 0);
-
-	-- Inverse of exposure time
-	-- This is a fixed-point fractional value
-	signal exposureinv : unsigned(11 downto 0);
-
-	-- S-inversion for target image
-	signal targetsinvpix1r : unsigned(11 downto 0);
-	signal targetsinvpix1g : unsigned(11 downto 0);
-	signal targetsinvpix1b : unsigned(11 downto 0);
-
-	signal targetsinvpix2r : unsigned(11 downto 0);
-	signal targetsinvpix2g : unsigned(11 downto 0);
-	signal targetsinvpix2b : unsigned(11 downto 0);
-
-	signal targetsinvpix3r : unsigned(11 downto 0);
-	signal targetsinvpix3g : unsigned(11 downto 0);
-	signal targetsinvpix3b : unsigned(11 downto 0);
-
-	signal targetsinvpix4r : unsigned(11 downto 0);
-	signal targetsinvpix4g : unsigned(11 downto 0);
-	signal targetsinvpix4b : unsigned(11 downto 0);
-
-	-- S-inversion for white image
-	signal whitesinvpix1r : unsigned(11 downto 0);
-	signal whitesinvpix1g : unsigned(11 downto 0);
-	signal whitesinvpix1b : unsigned(11 downto 0);
-
-	signal whitesinvpix2r : unsigned(11 downto 0);
-	signal whitesinvpix2g : unsigned(11 downto 0);
-	signal whitesinvpix2b : unsigned(11 downto 0);
-
-	signal whitesinvpix3r : unsigned(11 downto 0);
-	signal whitesinvpix3g : unsigned(11 downto 0);
-	signal whitesinvpix3b : unsigned(11 downto 0);
-
-	signal whitesinvpix4r : unsigned(11 downto 0);
-	signal whitesinvpix4g : unsigned(11 downto 0);
-	signal whitesinvpix4b : unsigned(11 downto 0);
-
-	-- Radiance target image
-	signal targetradpix1r : unsigned(11 downto 0);
-	signal targetradpix1g : unsigned(11 downto 0);
-	signal targetradpix1b : unsigned(11 downto 0);
-
-	signal targetradpix2r : unsigned(11 downto 0);
-	signal targetradpix2g : unsigned(11 downto 0);
-	signal targetradpix2b : unsigned(11 downto 0);
-
-	signal targetradpix3r : unsigned(11 downto 0);
-	signal targetradpix3g : unsigned(11 downto 0);
-	signal targetradpix3b : unsigned(11 downto 0);
-
-	signal targetradpix4r : unsigned(11 downto 0);
-	signal targetradpix4g : unsigned(11 downto 0);
-	signal targetradpix4b : unsigned(11 downto 0);
-
-	-- Radiance white image
-	signal whiteradpix1r : unsigned(11 downto 0);
-	signal whiteradpix1g : unsigned(11 downto 0);
-	signal whiteradpix1b : unsigned(11 downto 0);
-
-	signal whiteradpix2r : unsigned(11 downto 0);
-	signal whiteradpix2g : unsigned(11 downto 0);
-	signal whiteradpix2b : unsigned(11 downto 0);
-
-	signal whiteradpix3r : unsigned(11 downto 0);
-	signal whiteradpix3g : unsigned(11 downto 0);
-	signal whiteradpix3b : unsigned(11 downto 0);
-
-	signal whiteradpix4r : unsigned(11 downto 0);
-	signal whiteradpix4g : unsigned(11 downto 0);
-	signal whiteradpix4b : unsigned(11 downto 0);
-
-	-- Reflectance image
-	signal reflpix1r : unsigned(11 downto 0);
-	signal reflpix1g : unsigned(11 downto 0);
-	signal reflpix1b : unsigned(11 downto 0);
-
-	signal reflpix2r : unsigned(11 downto 0);
-	signal reflpix2g : unsigned(11 downto 0);
-	signal reflpix2b : unsigned(11 downto 0);
-
-	signal reflpix3r : unsigned(11 downto 0);
-	signal reflpix3g : unsigned(11 downto 0);
-	signal reflpix3b : unsigned(11 downto 0);
-
-	signal reflpix4r : unsigned(11 downto 0);
-	signal reflpix4g : unsigned(11 downto 0);
-	signal reflpix4b : unsigned(11 downto 0);
-
 begin
 
 	------------------------------------------
@@ -538,85 +411,6 @@ begin
 			whitesub3 <= to_unsigned(0, 12);
 			whitesub4 <= to_unsigned(0, 12);
 
-			-- Inversion coefficients
-			sinvr <= to_unsigned(1, 12);
-			sinvg <= to_unsigned(2, 12);
-			sinvb <= to_unsigned(3, 12);
-
-			-- Inverse of exposure time
-			exposureinv <= to_unsigned(4, 12);
-
-			-- S-inversion for target image
-			targetsinvpix1r <= to_unsigned(0, 12);
-			targetsinvpix1g <= to_unsigned(0, 12);
-			targetsinvpix1b <= to_unsigned(0, 12);
-			targetsinvpix2r <= to_unsigned(0, 12);
-			targetsinvpix2g <= to_unsigned(0, 12);
-			targetsinvpix2b <= to_unsigned(0, 12);
-			targetsinvpix3r <= to_unsigned(0, 12);
-			targetsinvpix3g <= to_unsigned(0, 12);
-			targetsinvpix3b <= to_unsigned(0, 12);
-			targetsinvpix4r <= to_unsigned(0, 12);
-			targetsinvpix4g <= to_unsigned(0, 12);
-			targetsinvpix4b <= to_unsigned(0, 12);
-
-			-- S-inversion for white image
-			whitesinvpix1r <= to_unsigned(0, 12);
-			whitesinvpix1g <= to_unsigned(0, 12);
-			whitesinvpix1b <= to_unsigned(0, 12);
-			whitesinvpix2r <= to_unsigned(0, 12);
-			whitesinvpix2g <= to_unsigned(0, 12);
-			whitesinvpix2b <= to_unsigned(0, 12);
-			whitesinvpix3r <= to_unsigned(0, 12);
-			whitesinvpix3g <= to_unsigned(0, 12);
-			whitesinvpix3b <= to_unsigned(0, 12);
-			whitesinvpix4r <= to_unsigned(0, 12);
-			whitesinvpix4g <= to_unsigned(0, 12);
-			whitesinvpix4b <= to_unsigned(0, 12);
-
-			-- Radiance target image
-			targetradpix1r <= to_unsigned(0, 12);
-			targetradpix1g <= to_unsigned(0, 12);
-			targetradpix1b <= to_unsigned(0, 12);
-			targetradpix2r <= to_unsigned(0, 12);
-			targetradpix2g <= to_unsigned(0, 12);
-			targetradpix2b <= to_unsigned(0, 12);
-			targetradpix3r <= to_unsigned(0, 12);
-			targetradpix3g <= to_unsigned(0, 12);
-			targetradpix3b <= to_unsigned(0, 12);
-			targetradpix4r <= to_unsigned(0, 12);
-			targetradpix4g <= to_unsigned(0, 12);
-			targetradpix4b <= to_unsigned(0, 12);
-
-			-- Radiance white image
-			whiteradpix1r <= to_unsigned(0, 12);
-			whiteradpix1g <= to_unsigned(0, 12);
-			whiteradpix1b <= to_unsigned(0, 12);
-			whiteradpix2r <= to_unsigned(0, 12);
-			whiteradpix2g <= to_unsigned(0, 12);
-			whiteradpix2b <= to_unsigned(0, 12);
-			whiteradpix3r <= to_unsigned(0, 12);
-			whiteradpix3g <= to_unsigned(0, 12);
-			whiteradpix3b <= to_unsigned(0, 12);
-			whiteradpix4r <= to_unsigned(0, 12);
-			whiteradpix4g <= to_unsigned(0, 12);
-			whiteradpix4b <= to_unsigned(0, 12);
-
-			-- Reflectance image
-			reflpix1r <= to_unsigned(0, 12);
-			reflpix1g <= to_unsigned(0, 12);
-			reflpix1b <= to_unsigned(0, 12);
-			reflpix2r <= to_unsigned(0, 12);
-			reflpix2g <= to_unsigned(0, 12);
-			reflpix2b <= to_unsigned(0, 12);
-			reflpix3r <= to_unsigned(0, 12);
-			reflpix3g <= to_unsigned(0, 12);
-			reflpix3b <= to_unsigned(0, 12);
-			reflpix4r <= to_unsigned(0, 12);
-			reflpix4g <= to_unsigned(0, 12);
-			reflpix4b <= to_unsigned(0, 12);
-
-
 			pipelinedelay <= (others => '0');
 
 			targetfirstrowhandled <= '0';
@@ -679,14 +473,6 @@ begin
 
 				whitefirstrowhandled <= whitefirstrowhandled;
 				whitereadrowodd <= whitereadrowodd;
-
-				-- Inversion coefficients
-				sinvr <= sinvr;
-				sinvg <= sinvg;
-				sinvb <= sinvb;
-
-				-- Inverse of exposure time
-				exposureinv <= exposureinv;
 
 				-- Image 1 pixel data for four pixels in 36-bit RGB format
 				read_done_target_delayed <= '0';
@@ -833,89 +619,6 @@ begin
 				else
 				end if;
 
-				----------------------------
-				-- S-inversion
-				----------------------------
-
-				-- S-inversion for target image
-				targetsinvpix1r <= img1pix1r * sinvr;
-				targetsinvpix1g <= img1pix1g * sinvg;
-				targetsinvpix1b <= img1pix1b * sinvb;
-				targetsinvpix2r <= img1pix2r * sinvr;
-				targetsinvpix2g <= img1pix2g * sinvg;
-				targetsinvpix2b <= img1pix2b * sinvb;
-				targetsinvpix3r <= img1pix3r * sinvr;
-				targetsinvpix3g <= img1pix3g * sinvg;
-				targetsinvpix3b <= img1pix3b * sinvb;
-				targetsinvpix4r <= img1pix4r * sinvr;
-				targetsinvpix4g <= img1pix4g * sinvg;
-				targetsinvpix4b <= img1pix4b * sinvb;
-
-				-- S-inversion for white image
-				whitesinvpix1r <= img2pix1r * sinvr;
-				whitesinvpix1g <= img2pix1g * sinvg;
-				whitesinvpix1b <= img2pix1b * sinvb;
-				whitesinvpix2r <= img2pix2r * sinvr;
-				whitesinvpix2g <= img2pix2g * sinvg;
-				whitesinvpix2b <= img2pix2b * sinvb;
-				whitesinvpix3r <= img2pix3r * sinvr;
-				whitesinvpix3g <= img2pix3g * sinvg;
-				whitesinvpix3b <= img2pix3b * sinvb;
-				whitesinvpix4r <= img2pix4r * sinvr;
-				whitesinvpix4g <= img2pix4g * sinvg;
-				whitesinvpix4b <= img2pix4b * sinvb;
-
-				----------------------------
-				-- Radiance image
-				----------------------------
-
-				-- Radiance target image
-				targetradpix1r <= targetsinvpix1r * exposureinv;
-				targetradpix1g <= targetsinvpix1g * exposureinv;
-				targetradpix1b <= targetsinvpix1b * exposureinv;
-				targetradpix2r <= targetsinvpix2r * exposureinv;
-				targetradpix2g <= targetsinvpix2g * exposureinv;
-				targetradpix2b <= targetsinvpix2b * exposureinv;
-				targetradpix3r <= targetsinvpix3r * exposureinv;
-				targetradpix3g <= targetsinvpix3g * exposureinv;
-				targetradpix3b <= targetsinvpix3b * exposureinv;
-				targetradpix4r <= targetsinvpix4r * exposureinv;
-				targetradpix4g <= targetsinvpix4g * exposureinv;
-				targetradpix4b <= targetsinvpix4b * exposureinv;
-
-				-- Radiance white image
-				whiteradpix1r <= whitesinvpix1r * exposureinv;
-				whiteradpix1g <= whitesinvpix1g * exposureinv;
-				whiteradpix1b <= whitesinvpix1b * exposureinv;
-				whiteradpix2r <= whitesinvpix2r * exposureinv;
-				whiteradpix2g <= whitesinvpix2g * exposureinv;
-				whiteradpix2b <= whitesinvpix2b * exposureinv;
-				whiteradpix3r <= whitesinvpix3r * exposureinv;
-				whiteradpix3g <= whitesinvpix3g * exposureinv;
-				whiteradpix3b <= whitesinvpix3b * exposureinv;
-				whiteradpix4r <= whitesinvpix4r * exposureinv;
-				whiteradpix4g <= whitesinvpix4g * exposureinv;
-				whiteradpix4b <= whitesinvpix4b * exposureinv;
-
-				----------------------------
-				-- Reflectance image
-				----------------------------
-
-				-- Reflectance image
-				-- This needs to be changed to division
-				reflpix1r <= targetradpix1r * whiteradpix1r;
-				reflpix1g <= targetradpix1g * whiteradpix1g;
-				reflpix1b <= targetradpix1b * whiteradpix1b;
-				reflpix2r <= targetradpix2r * whiteradpix2r;
-				reflpix2g <= targetradpix2g * whiteradpix2g;
-				reflpix2b <= targetradpix2b * whiteradpix2b;
-				reflpix3r <= targetradpix3r * whiteradpix3r;
-				reflpix3g <= targetradpix3g * whiteradpix3g;
-				reflpix3b <= targetradpix3b * whiteradpix3b;
-				reflpix4r <= targetradpix4r * whiteradpix4r;
-				reflpix4g <= targetradpix4g * whiteradpix4g;
-				reflpix4b <= targetradpix4b * whiteradpix4b;
-
 			else
 			end if;
 			
@@ -936,21 +639,31 @@ begin
 				&	std_logic_vector(whitesub2)
 				&	std_logic_vector(whitesub3)
 				&	std_logic_vector(whitesub4);
-
-	respix1r <= reflpix1r;
-	respix1g <= reflpix1g;
-	respix1b <= reflpix1b;
-
-	respix2r <= reflpix2r;
-	respix2g <= reflpix2g;
-	respix2b <= reflpix2b;
-
-	respix3r <= reflpix3r;
-	respix3g <= reflpix3g;
-	respix3b <= reflpix3b;
-
-	respix4r <= reflpix4r;
-	respix4g <= reflpix4g;
-	respix4b <= reflpix4b;
 	
+	targetpix1r <= img1pix1r;
+	targetpix1g <= img1pix1g;
+	targetpix1b <= img1pix1b;
+	targetpix2r <= img1pix2r;
+	targetpix2g <= img1pix2g;
+	targetpix2b <= img1pix2b;
+	targetpix3r <= img1pix3r;
+	targetpix3g <= img1pix3g;
+	targetpix3b <= img1pix3b;
+	targetpix4r <= img1pix4r;
+	targetpix4g <= img1pix4g;
+	targetpix4b <= img1pix4b;
+
+	whitepix1r <= img2pix1r;
+	whitepix1g <= img2pix1g;
+	whitepix1b <= img2pix1b;
+	whitepix2r <= img2pix2r;
+	whitepix2g <= img2pix2g;
+	whitepix2b <= img2pix2b;
+	whitepix3r <= img2pix3r;
+	whitepix3g <= img2pix3g;
+	whitepix3b <= img2pix3b;
+	whitepix4r <= img2pix4r;
+	whitepix4g <= img2pix4g;
+	whitepix4b <= img2pix4b;
+
 end architecture;
