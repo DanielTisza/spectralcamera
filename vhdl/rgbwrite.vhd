6456b0605f7962451a40019620b470f31a4f2683
daniel_tisza <daniel.tisza@tietolaite.com>
2023-09-06 22:59:35 +0300

    Work on rgbwrite.vhd

--- vhdl/rgbwrite.vhd
+++ vhdl/rgbwrite.vhd
@@ -30,9 +30,7 @@ entity rgbwrite is
 		resetn	: in std_logic;
 
 		-- Input data signals
-		imgsync_ena : in std_logic;
-		hsync_ena : in std_logic;
-		vsync_ena : in std_logic;
+		read_done_c : in std_logic;
 
 		-- Writing channel signals
 		write_ready : in std_logic;
@@ -40,7 +38,9 @@ entity rgbwrite is
 		write_addr : out std_logic_vector(C_M_AXI_ADDR_WIDTH-1 downto 0);
 		write_data : out std_logic_vector(C_M_AXI_DATA_WIDTH-1 downto 0);
 
-		-- Step 1 calculation result
+		-- Pipeline calculation result
+		-- 4 RGB pixels with double precision floating point values
+		-- 12 * 64-bit data
 		res1pix1r : in unsigned(63 downto 0);
 		res1pix1g : in unsigned(63 downto 0);
 		res1pix1b : in unsigned(63 downto 0);
@@ -64,8 +64,7 @@ architecture rtl of rgbwrite is
 	signal write_addr_int : unsigned(C_M_AXI_ADDR_WIDTH-1 downto 0);
 	signal write_ena_int : std_logic;
 
-	signal pipelinedelay : std_logic_vector(3 downto 0);
-	signal dstoffset : unsigned(23 downto 0);	
+	signal pipelinedelay : unsigned(9 downto 0);
 
 	signal firstrowhandled : std_logic;
 	signal readrowodd : std_logic;
@@ -85,8 +84,8 @@ architecture rtl of rgbwrite is
 
 	signal rowIdx : unsigned(10 downto 0);
 	signal colIdx : unsigned(11 downto 0);
-
-	signal data_shift_ena : std_logic;
+	signal hsync_ena : std_logic;
+	signal vsync_ena : std_logic;
 
 	-- Sequence
 	signal writeseqstatebits : std_logic_vector(7 downto 0);
@@ -97,6 +96,8 @@ architecture rtl of rgbwrite is
 	-- Single write
 	signal writestatebits : std_logic_vector(7 downto 0);
 	signal writenext_done : std_logic;
+
+	signal capture_ena : std_logic;
 
 begin
     
@@ -111,8 +112,7 @@ begin
 
 		if (resetn='0') then
 
-			pipelinedelay <= (others => '0');
-			dstoffset <= to_unsigned(0, 24);
+			pipelinedelay <= to_unsigned(0, 10);
 
 			-- Writing channel signals
 			write_addr_int <= to_unsigned(0, C_M_AXI_ADDR_WIDTH);
@@ -120,9 +120,6 @@ begin
 
 			firstrowhandled <= '0';
 			readrowodd <= '0';
-
-			-- Pipeline processing delay shift register trigger
-			pipelinedelay <= (others => '0');
 
 			pix1r <= to_unsigned(0, 64);
 			pix1g <= to_unsigned(0, 64);
@@ -139,8 +136,8 @@ begin
 
 			rowIdx <= to_unsigned(0,11);
 			colIdx <= to_unsigned(0,12);
-
-			data_shift_ena <= '0';
+			hsync_ena <= '0';
+			vsync_ena <= '0';
 
 			writeseqstatebits <= "00000001";
 			writeseq_done_int <= '0';
@@ -150,6 +147,8 @@ begin
 			writestatebits <= "00000001";
 			writenext_done <= '0';
 
+			capture_ena <= '0';
+
 		else
 
 			if (clk'event and clk='1') then
@@ -158,14 +157,8 @@ begin
 				write_addr_int <= write_addr_int;
 				write_ena_int <= '0';
 
-				pipelinedelay <= pipelinedelay(pipelinedelay'length-2 downto 0) & pipelinedelay(pipelinedelay'length-1);
-				dstoffset <= dstoffset;
-
 				firstrowhandled <= firstrowhandled;
 				readrowodd <= readrowodd;
-
-				-- Pipeline processing delay shift register trigger
-				pipelinedelay <= pipelinedelay(pipelinedelay'length-2 downto 0) & '0';
 
 				pix1r <= pix1r;
 				pix1g <= pix1g;
@@ -182,8 +175,8 @@ begin
 
 				rowIdx <= rowIdx;
 				colIdx <= colIdx;
-
-				data_shift_ena <= data_shift_ena;
+				hsync_ena <= '0';
+				vsync_ena <= '0';
 
 				writeseqstatebits <= writeseqstatebits;
 				writeseq_done_int <= '0';
@@ -192,6 +185,36 @@ begin
 
 				writestatebits <= writestatebits;
 				writenext_done <= '0';
+
+				pipelinedelay <= pipelinedelay;
+				capture_ena <= '0';
+
+				--------------------------------------
+				-- Pipeline delay counter
+				--------------------------------------
+
+				-- Counter is reloaded when latest data is made available to
+				-- pipeline at pipeline input.
+				--
+				-- When pipeline counter reaches 1, then pipeline processing
+				-- has completed and processed data is available at 
+				-- pipeline output.
+
+				if (read_done_c='1') then
+					pipelinedelay <= to_unsigned(16,10);
+				else
+
+					if (pipelinedelay > to_unsigned(0,10)) then
+						pipelinedelay <= pipelinedelay - to_unsigned(1,10);
+					else
+					end if;
+
+				end if;
+
+				if (pipelinedelay = to_unsigned(1,10)) then
+					capture_ena <= '1';
+				else
+				end if;
 
 				-- Result image RGB pixels with double-precision floating point
 				-- has 64-bit pixels (8 bytes)
@@ -213,29 +236,7 @@ begin
 				-- 2590 x 1942
 				-- 2590 pixels * 8 bytes = 20720 (0x50F0)
 				-- 2590 * 1942 * 8 bytes = 40238240 bytes
-
-
-				-- Select data to write
-				-- Make 144 bits shift register where highest 64-bits connected
-				-- to write port
-				if (data_shift_ena='1') then
-
-					pix1r <= pix1g;
-					pix1g <= pix1b;
-					pix1b <= pix2r;
-					pix2r <= pix2g;
-					pix2g <= pix2b;
-					pix2b <= pix3r;
-					pix3r <= pix3g;
-					pix3g <= pix3b;
-					pix3b <= pix4r;
-					pix4r <= pix4g;
-					pix4g <= pix4b;
-					-- pix4b <= pix4b;
-
-				else
-				end if;
-
+				
 				-- Result image
 				-- 3EB3FB00		first row
 
@@ -254,9 +255,8 @@ begin
 									+ 	rowIdx * to_unsigned(20720, 15)
 									+	colIdx * to_unsigned(8, 4);
 
-
 				--------------------------------------
-				-- Sequence
+				-- Write sequence for 12 writes
 				--------------------------------------
 				case writeseqstatebits is
 
@@ -267,24 +267,32 @@ begin
 						-- Waiting for data to arrive from pipeline
 
 						writecounter <= to_unsigned(12, 4);
-						writeseqstatebits <= "00000010";
+
+						if (capture_ena='1') then
+							writenext_ena <= '1';
+							writeseqstatebits <= "00000010";
+						else
+						end if;
 
 					when "00000010" =>
 
-						-- Start writing if there are data left to write
-						-- Otherwise stop writing
+						-- Write is in progress,
+						-- waiting for writing to complete
 						if (writenext_done='1') then
-							writeseqstatebits <= "00000001";
+							writecounter <= writecounter - to_unsigned(1, 4);
+							writeseqstatebits <= "00000100";
 						else
 						end if;
 
 					when "00000100" =>
 
-						-- Waiting for writing
-						if (writenext_done='1') then
-							writeseqstatebits <= "00000010";
-							writecounter <= writecounter - to_unsigned(1, 4);
+						-- Check if need to write more
+						-- or stop writing
+						if (writecounter = to_unsigned(0,4)) then
+							writeseqstatebits <= "00000001";
 						else
+							writenext_ena <= '1';
+							writeseqstatebits <= "00000010";
 						end if;
 
 					when others =>
@@ -328,7 +336,66 @@ begin
 						null;
 
 				end case;
-				
+
+
+				--------------------------------------
+				-- Prepare data to write
+				--------------------------------------
+
+				-- Make 144 bits shift register where 
+				-- highest 64-bits connected to write port
+
+				if (writenext_done='1') then
+
+					pix1r <= pix1g;
+					pix1g <= pix1b;
+					pix1b <= pix2r;
+					pix2r <= pix2g;
+					pix2g <= pix2b;
+					pix2b <= pix3r;
+					pix3r <= pix3g;
+					pix3g <= pix3b;
+					pix3b <= pix4r;
+					pix4r <= pix4g;
+					pix4g <= pix4b;
+					-- pix4b <= pix4b;
+				else
+				end if;
+
+
+				--------------------------------------
+				-- Column index and row index for write address
+				--------------------------------------
+
+				-- Increment column index or
+				-- reset column index when maximum reached
+				if (writenext_done='1') then
+
+					-- 2590 pixels / 4 pixels per 64 bits transfer
+					-- 2590/4 = 647.5 = 648
+					if (colIdx=to_unsigned(648,12)) then
+						colIdx <= to_unsigned(0,12);
+						hsync_ena <= '1';
+					else
+						colIdx <= colIdx + to_unsigned(1,12);
+					end if;
+
+				else
+				end if;
+
+				-- Increment row index or
+				-- reset row index when maximum reached
+				if (hsync_ena='1') then
+
+					if (rowIdx=to_unsigned(1942,11)) then
+						rowIdx <= to_unsigned(0,11);
+						vsync_ena <= '1';
+					else
+						rowIdx <= rowIdx + to_unsigned(1,11);
+					end if;
+					
+				else 
+				end if;
 
 			else
 			end if;
