68dc7aaff10abf00418264f4aede832652037d97
daniel_tisza <daniel.tisza@tietolaite.com>
2023-09-27 07:28:55 +0300

    Change to do calculations in targetwhitediv in two phases.

--- vhdl/targetwhitediv.vhd
+++ vhdl/targetwhitediv.vhd
@@ -22,6 +22,7 @@ entity targetwhitediv is
 
 		-- Clock and reset
 		clk : in std_logic;
+		resetn	: in std_logic;
 
 		-- Input data signals
 		img1pix1r : in STD_LOGIC_VECTOR ( 63 downto 0 );
@@ -70,6 +71,44 @@ end targetwhitediv;
 -- Describe the contents of this "chip"
 architecture rtl of targetwhitediv is
 
+	signal muxphase : std_logic;
+
+	-- Divider input signals
+	signal divinpix1r : STD_LOGIC_VECTOR ( 63 downto 0 );
+	signal divinpix1g : STD_LOGIC_VECTOR ( 63 downto 0 );
+	signal divinpix1b : STD_LOGIC_VECTOR ( 63 downto 0 );
+	signal divinpix2r : STD_LOGIC_VECTOR ( 63 downto 0 );
+	signal divinpix2g : STD_LOGIC_VECTOR ( 63 downto 0 );
+	signal divinpix2b : STD_LOGIC_VECTOR ( 63 downto 0 );
+	signal divinpix3r : STD_LOGIC_VECTOR ( 63 downto 0 );
+	signal divinpix3g : STD_LOGIC_VECTOR ( 63 downto 0 );
+	signal divinpix3b : STD_LOGIC_VECTOR ( 63 downto 0 );
+	signal divinpix4r : STD_LOGIC_VECTOR ( 63 downto 0 );
+	signal divinpix4g : STD_LOGIC_VECTOR ( 63 downto 0 );
+	signal divinpix4b : STD_LOGIC_VECTOR ( 63 downto 0 );
+
+	-- Divider output signals
+	signal divoutpix1r : STD_LOGIC_VECTOR ( 63 downto 0 );
+	signal divoutpix1g : STD_LOGIC_VECTOR ( 63 downto 0 );
+	signal divoutpix1b : STD_LOGIC_VECTOR ( 63 downto 0 );
+	signal divoutpix2r : STD_LOGIC_VECTOR ( 63 downto 0 );
+	signal divoutpix2g : STD_LOGIC_VECTOR ( 63 downto 0 );
+	signal divoutpix2b : STD_LOGIC_VECTOR ( 63 downto 0 );
+
+	-- Output buffer
+	signal reflpix1r_int : STD_LOGIC_VECTOR ( 63 downto 0 );
+	signal reflpix1g_int : STD_LOGIC_VECTOR ( 63 downto 0 );
+	signal reflpix1b_int : STD_LOGIC_VECTOR ( 63 downto 0 );
+	signal reflpix2r_int : STD_LOGIC_VECTOR ( 63 downto 0 );
+	signal reflpix2g_int : STD_LOGIC_VECTOR ( 63 downto 0 );
+	signal reflpix2b_int : STD_LOGIC_VECTOR ( 63 downto 0 );
+	signal reflpix3r_int : STD_LOGIC_VECTOR ( 63 downto 0 );
+	signal reflpix3g_int : STD_LOGIC_VECTOR ( 63 downto 0 );
+	signal reflpix3b_int : STD_LOGIC_VECTOR ( 63 downto 0 );
+	signal reflpix4r_int : STD_LOGIC_VECTOR ( 63 downto 0 );
+	signal reflpix4g_int : STD_LOGIC_VECTOR ( 63 downto 0 );
+	signal reflpix4b_int : STD_LOGIC_VECTOR ( 63 downto 0 );
+
 	component rgbdoublediv is
 	port (
 		-- Clock and reset
@@ -94,62 +133,193 @@ architecture rtl of targetwhitediv is
 
 begin
 
+	mux_proc : process(
+		clk,
+		resetn
+	)
+	begin
+
+		if (resetn='0') then
+
+			muxphase <= '0';
+
+			divinpix1r <= X"0000000000000000";
+			divinpix1g <= X"0000000000000000";
+			divinpix1b <= X"0000000000000000";
+			divinpix2r <= X"0000000000000000";
+			divinpix2g <= X"0000000000000000";
+			divinpix2b <= X"0000000000000000";
+			divinpix3r <= X"0000000000000000";
+			divinpix3g <= X"0000000000000000";
+			divinpix3b <= X"0000000000000000";
+			divinpix4r <= X"0000000000000000";
+			divinpix4g <= X"0000000000000000";
+			divinpix4b <= X"0000000000000000";
+
+			reflpix1r_int <= X"0000000000000000";
+			reflpix1g_int <= X"0000000000000000";
+			reflpix1b_int <= X"0000000000000000";
+			reflpix2r_int <= X"0000000000000000";
+			reflpix2g_int <= X"0000000000000000";
+			reflpix2b_int <= X"0000000000000000";
+			reflpix3r_int <= X"0000000000000000";
+			reflpix3g_int <= X"0000000000000000";
+			reflpix3b_int <= X"0000000000000000";
+			reflpix4r_int <= X"0000000000000000";
+			reflpix4g_int <= X"0000000000000000";
+			reflpix4b_int <= X"0000000000000000";
+
+		else
+
+			if (clk'event and clk='1') then
+
+				-- Default values
+				reflpix1r_int <= reflpix1r_int;
+				reflpix1g_int <= reflpix1g_int;
+				reflpix1b_int <= reflpix1b_int;
+				reflpix2r_int <= reflpix2r_int;
+				reflpix2g_int <= reflpix2g_int;
+				reflpix2b_int <= reflpix2b_int;
+				reflpix3r_int <= reflpix3r_int;
+				reflpix3g_int <= reflpix3g_int;
+				reflpix3b_int <= reflpix3b_int;
+				reflpix4r_int <= reflpix4r_int;
+				reflpix4g_int <= reflpix4g_int;
+				reflpix4b_int <= reflpix4b_int;
+
+				muxphase <= not(muxphase);
+
+				if (muxphase='0') then
+
+					-- Input first 2 pixels from both images
+					divinpix1r <= img1pix1r;
+					divinpix1g <= img1pix1g;
+					divinpix1b <= img1pix1b;
+					divinpix2r <= img1pix2r;
+					divinpix2g <= img1pix2g;
+					divinpix2b <= img1pix2b;
+
+					divinpix3r <= img2pix1r;
+					divinpix3g <= img2pix1g;
+					divinpix3b <= img2pix1b;
+					divinpix4r <= img2pix2r;
+					divinpix4g <= img2pix2g;
+					divinpix4b <= img2pix2b;
+
+					-- Output first 2 pixels from both images
+					reflpix1r_int <= divoutpix1r;
+					reflpix1g_int <= divoutpix1g;
+					reflpix1b_int <= divoutpix1b;
+					reflpix2r_int <= divoutpix2r;
+					reflpix2g_int <= divoutpix2g;
+					reflpix2b_int <= divoutpix2b;
+					
+				else
+
+					-- Input last 2 pixels from both images
+					divinpix1r <= img1pix3r;
+					divinpix1g <= img1pix3g;
+					divinpix1b <= img1pix3b;
+					divinpix2r <= img1pix4r;
+					divinpix2g <= img1pix4g;
+					divinpix2b <= img1pix4b;
+
+					divinpix3r <= img2pix3r;
+					divinpix3g <= img2pix3g;
+					divinpix3b <= img2pix3b;
+					divinpix4r <= img2pix4r;
+					divinpix4g <= img2pix4g;
+					divinpix4b <= img2pix4b;
+
+					-- Output last 2 pixels from both images
+					reflpix3r_int <= divoutpix1r;
+					reflpix3g_int <= divoutpix1g;
+					reflpix3b_int <= divoutpix1b;
+					reflpix4r_int <= divoutpix2r;
+					reflpix4g_int <= divoutpix2g;
+					reflpix4b_int <= divoutpix2b;
+
+				end if;
+
+			else
+			end if;
+		end if;
+
+	end process;
+
 	------------------------------------------
-	-- Divide target image pixels 
-	-- with white image pixels
+	-- Dividers
 	------------------------------------------
 
 	pix1div : rgbdoublediv port map(
 		clk => clk,
-		pixr1 => img1pix1r,
-		pixg1 => img1pix1g,
-		pixb1 => img1pix1b,
-		pixr2 => img2pix1r,
-		pixg2 => img2pix1g,
-		pixb2 => img2pix1b,
-		pixr => reflpix1r,
-		pixg => reflpix1g,
-		pixb => reflpix1b
+		pixr1 => divinpix1r,
+		pixg1 => divinpix1g,
+		pixb1 => divinpix1b,
+		pixr2 => divinpix3r,
+		pixg2 => divinpix3g,
+		pixb2 => divinpix3b,
+		pixr => divoutpix1r,
+		pixg => divoutpix1g,
+		pixb => divoutpix1b
 	);
 
 	pix2div : rgbdoublediv port map(
 		clk => clk,
-		pixr1 => img1pix2r,
-		pixg1 => img1pix2g,
-		pixb1 => img1pix2b,
-		pixr2 => img2pix2r,
-		pixg2 => img2pix2g,
-		pixb2 => img2pix2b,
-		pixr => reflpix2r,
-		pixg => reflpix2g,
-		pixb => reflpix2b
+		pixr1 => divinpix2r,
+		pixg1 => divinpix2g,
+		pixb1 => divinpix2b,
+		pixr2 => divinpix4r,
+		pixg2 => divinpix4g,
+		pixb2 => divinpix4b,
+		pixr => divoutpix2r,
+		pixg => divoutpix2g,
+		pixb => divoutpix2b
 	);
 
-	pix3div : rgbdoublediv port map(
-		clk => clk,
-		pixr1 => img1pix3r,
-		pixg1 => img1pix3g,
-		pixb1 => img1pix3b,
-		pixr2 => img2pix3r,
-		pixg2 => img2pix3g,
-		pixb2 => img2pix3b,
-		pixr => reflpix3r,
-		pixg => reflpix3g,
-		pixb => reflpix3b
-	);
+--	pix3div : rgbdoublediv port map(
+--		clk => clk,
+--		pixr1 => img1pix3r,
+--		pixg1 => img1pix3g,
+--		pixb1 => img1pix3b,
+--		pixr2 => img2pix3r,
+--		pixg2 => img2pix3g,
+--		pixb2 => img2pix3b,
+--		pixr => reflpix3r,
+--		pixg => reflpix3g,
+--		pixb => reflpix3b
+--	);
+--
+--	pix4div : rgbdoublediv port map(
+--		clk => clk,
+--		pixr1 => img1pix4r,
+--		pixg1 => img1pix4g,
+--		pixb1 => img1pix4b,
+--		pixr2 => img2pix4r,
+--		pixg2 => img2pix4g,
+--		pixb2 => img2pix4b,
+--		pixr => reflpix4r,
+--		pixg => reflpix4g,
+--		pixb => reflpix4b
+--	);
 
-	pix4div : rgbdoublediv port map(
-		clk => clk,
-		pixr1 => img1pix4r,
-		pixg1 => img1pix4g,
-		pixb1 => img1pix4b,
-		pixr2 => img2pix4r,
-		pixg2 => img2pix4g,
-		pixb2 => img2pix4b,
-		pixr => reflpix4r,
-		pixg => reflpix4g,
-		pixb => reflpix4b
-	);
 
+	--------------------------
+	-- Continuous connections
+	--------------------------
+
+	reflpix1r <= reflpix1r_int;
+	reflpix1g <= reflpix1g_int;
+	reflpix1b <= reflpix1b_int;
+	reflpix2r <= reflpix2r_int;
+	reflpix2g <= reflpix2g_int;
+	reflpix2b <= reflpix2b_int;
+
+	reflpix3r <= reflpix3r_int;
+	reflpix3g <= reflpix3g_int;
+	reflpix3b <= reflpix3b_int;
+	reflpix4r <= reflpix4r_int;
+	reflpix4g <= reflpix4g_int;
+	reflpix4b <= reflpix4b_int;
 
 end architecture;
